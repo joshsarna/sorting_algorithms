c9dc89e7da1656346774e870b4f9504e139606cb
joshsarna <joshsarna@gmail.com>
2019-04-06 22:47:50 -0500

    fix insertion_sort in ide

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,7 +1,7 @@
 def insertion_sort(numbers)
   numbers.length.times do |i|
     displacement = 0
-    while (i - displacement > 0 && numbers[i - displacement] < numbers[i - displacement - 1]
+    while (i - displacement > 0 && numbers[i - displacement] < numbers[i - displacement - 1])
       numbers[i - displacement], numbers[i - displacement - 1] = numbers[i - displacement - 1], numbers[i - displacement]
       displacement += 1
     end
