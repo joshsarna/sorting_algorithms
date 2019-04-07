a88138af8987eb097ddacfe23e55c5b4eba4d390
joshsarna <joshsarna@gmail.com>
2019-04-06 22:47:04 -0500

    add whiteboard insertion_sort

---
+++ insertion_sort.rb
@@ -0,0 +1,10 @@
+def insertion_sort(numbers)
+  numbers.length.times do |i|
+    displacement = 0
+    while (i - displacement > 0 && numbers[i - displacement] < numbers[i - displacement - 1]
+      numbers[i - displacement], numbers[i - displacement - 1] = numbers[i - displacement - 1], numbers[i - displacement]
+      displacement += 1
+    end
+  end
+  numbers
+end
